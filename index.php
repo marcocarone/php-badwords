87fd8d0b7768d0943be1d98e4cf508cf49d746c3
Marco <marco.carone@libero.it>
2020-02-20 17:21:47 +0100

    fix testo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 Visualizzare a schermo il paragrafo con la relative lunghezza e sostituire la badword passata in GET con tre *. -->
 <?php
     // Creo una variabile testo
-    $testo = " Cavallino arri, arrò, prendi la biada che ti do, prendi i ferri che ti metto per andare a San Francesco. A San Francesco c’è una via che ti porta a casa mia. A casa mia c’è un altare con tre monache a lavorare, una cuce, una taglia, una fa cappelli di paglia; la più piccola e vecchietta Santa Barbara benedetta.";
+    $testo = "Le stazioni si somigliano tutte; poco importa se le luci non riescono a rischiarare più in là del loro alone sbavato, tanto questo è un ambiente che tu conosci a memoria, con l'odore di treno che resta anche dopo che tutti i treni sono partiti, l'odore speciale delle stazioni dopo che è partito l'ultimo treno. Le luci della stazione e le frasi che stai leggendo sembra abbiano il compito di dissolvere più che di indicare le cose affioranti da un velo di buio e di nebbia. Io sono sbarcato in questa stazione stasera per la prima volta in vita mia e già mi sembra d'averci passato una vita, entrando e uscendo da questo bar, passando dall'odore della pensilina all'odore di segatura bagnata dei gabinetti, tutto mescolato in un unico odore che è quello dell'attesa, l'odore delle cabine telefoniche quando non resta che recuperare i gettoni perché il numero chiamato non dà segno di vita.";
 
     // parola che viene inserita tramite parametro GET per esempio index.php?parola=ferri
     $parola_censurata = $_GET["parola"];
